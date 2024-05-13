56ea019cf97fa31748875f3ad0acad56c8a9354f
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-13 12:36:58 +0200

    snack terminato

---
+++ snack-3/script.js
@@ -0,0 +1,11 @@
+//Crea un array vuoto.Chiedi per 6 volte all'utente di inserire un numero,se è dispari inseriscilo nell'array.
+
+const numeriUtente = [];
+
+for ( i=0 ; i<6 ; i++){
+    const numeroUtente = Number.parseInt(prompt("Inserisci un numero intero"));
+    if (numeroUtente % 2 == 1){
+        numeriUtente.push(numeroUtente);
+        console.log(numeriUtente );
+    }
+};
