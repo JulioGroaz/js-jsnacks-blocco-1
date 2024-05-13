8faa1674b70ad28344d75a56b0d79fc03e9169d0
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-13 11:54:51 +0200

    fatto

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,10 @@
 //L'utente inserisce due numeri in successione, con due prompt.Il software stampa il maggiore.
 
-const primoNumero = prompt("Inserisci un numero");
-const secondoNumero = prompt("Inserisci il secondo numero")
+const primoNumero = Number.parseInt(prompt("Inserisci un numero"));
+const secondoNumero = Number.parseInt(prompt("Inserisci il secondo numero"))
+
+if (primoNumero>secondoNumero) {
+    console.log(primoNumero);
+} else {
+    console.log(secondoNumero);
+}
