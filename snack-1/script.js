f58e79a2d6d169224dfc9f324002fb281a23456e
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-13 11:46:38 +0200

    Creato i due file, dichiarato le variabili.

---
+++ snack-1/script.js
@@ -0,0 +1,4 @@
+//L'utente inserisce due numeri in successione, con due prompt.Il software stampa il maggiore.
+
+const primoNumero = prompt("Inserisci un numero");
+const secondoNumero = prompt("Inserisci il secondo numero")
