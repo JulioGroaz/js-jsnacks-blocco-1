9d2bed90cca22af3004b7c72d96bc882f779703c
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-14 11:29:34 +0200

    Creazione file

---
+++ snack-5/script.js
@@ -0,0 +1,7 @@
+//Snack7: Fai inserire un numero, che chiameremo N, all'utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
+
+const numeroUtente =Number.parsInt(prompt("Inserisci un numero intero"),10);
+
+for (i = 0; i < numeroUtente ; i++){
+    let numeroRandom = Math.floor(Math.random())
+}
