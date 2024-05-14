3fbc05967ed02b7d03e24a02032d5595ea04b345
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-14 10:53:05 +0200

    Esercizio fatto

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,2 +1,12 @@
 //Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari:
 
+const numeriArray = [5,10,15,20,25,30,35];
+
+let somma =0;
+
+for(let i =0; i < numbers.length; i++) {
+    if (i %2 !== 0) {
+        sum = sum + numeriArray [i];
+    }
+}
+console.log("somma");
