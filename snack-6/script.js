dd589b2582f88efead258f7cf4d06c074d2e0ec0
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-14 11:37:33 +0200

    div creati

--- snack-6/script.js
+++ snack-6/script.js
@@ -1,3 +1,5 @@
 //Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
 
 
+const Numeri = [1,34,55,64,7,90,2,39];
+
